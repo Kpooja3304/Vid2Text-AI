c605e0fc28dd761ab2d06f8a11bbafcd790b6c6e
Kpooja3304 <139441759+Kpooja3304@users.noreply.github.com>
2025-03-28 20:17:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,3 @@ def transcribe():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Ensure dynamic port assignment
-    app.run(host="0.0.0.0", port=port)
